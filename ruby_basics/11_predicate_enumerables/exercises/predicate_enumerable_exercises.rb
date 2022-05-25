8808dde84aa11f26f1ebda906961296b4c4384b6
Aarya <amathrej@asu.edu>
2022-05-24 21:00:57 -0700

    Write solution for coffee_drink? & correct_guess?

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,9 +1,11 @@
 def coffee_drink?(drink_list)
   # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  drink_list.include?('coffee') || drink_list.include?('espresso')
 end
 
 def correct_guess?(guess_list, answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+  guess_list.any? {|number| number == answer}
 end
 
 def twenty_first_century_years?(year_list)
